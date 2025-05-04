26ccafccca63b57315949a92d401e13766f36266
thuanntsolution <thuan.nguyenthanh.contact@gmail.com>
2025-05-04 22:35:32 +0700

    chore(env): edit config vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
